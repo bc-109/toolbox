3cb9f79ffde116cb852fc1a19fe5e06f92b84fa6
Santu <t.ottavi@medi.fr>
2020-05-23 22:31:40 +0200

    AsyncIO TCP lib

---
+++ src/asyncio_tcp_toolbox.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#===============================================================================
+# Python3 Toolbox
+# (c) 2011-2020 by Toussaint OTTAVI, t.ottavi@medi.fr
+#===============================================================================
+#
+# AsyncIO TCP client and server
+#
+#===============================================================================
+
+#===============================================================================
+#    This is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+Name = "Python Toolbox - AsyncIO TCP client and server library"
+Version = "0.1"
+VersionDate = "23/05/2020"
+
+
+
+
+
+################################################################################
+#                                                                              #
+#                              M A I N                                         #
+#                                                                              # 
+################################################################################
+
+if __name__ == "__main__":
+  print ("%s - (c) Toussaint OTTAVI, bc-109 Soft, t.ottavi@medi.fr" %(Name))
+  print ("Version %s, date : %s" % (Version, VersionDate))
+  print ("This is a library, to be called from other modules. It does nothing by itself.")
